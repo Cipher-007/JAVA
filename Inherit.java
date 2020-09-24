32d621619241d51f7ebe9014230d5aa6614d47ef
Gaurav Deshmukh <67900669+Cipher-007@users.noreply.github.com>
2020-09-24 14:47:26 +0530

    new

---
+++ Inherit.java
@@ -0,0 +1,42 @@
+class A
+{
+    int a;
+    void set_a(int i)
+    {
+        a=i;
+    }
+    void show_a()
+    {
+        System.out.println("The value of a: "+a);
+    }
+}
+class B extends A
+{
+    int b;
+    void set_b(int i)
+    {
+        b=i;
+    }
+    void show_b()
+    {
+        System.out.println("The value of b: "+b);
+    }
+    void mul()
+    {
+        int c;
+        c=a*b;
+        System.out.println("The value of c: "+c);
+    }
+}
+public class Inherit
+{
+     public static void main(String[] args)
+     {
+         B obj_B=new B();
+         obj_B.set_a(10);
+         obj_B.set_b(20);
+         obj_B.show_a();
+         obj_B.show_b();
+         obj_B.mul();
+     }
+}
